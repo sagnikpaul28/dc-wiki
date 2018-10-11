1c946a98a9551c87d474253d8722a7004a4cf133
Sagnik Paul <sagnik.paul@nowfloats.com>
2018-10-11 16:56:10 +0530

    updated single characters page

--- client/src/app/components/SingleCharacter.js
+++ client/src/app/components/SingleCharacter.js
@@ -14,23 +14,46 @@ export class SingleCharacter extends React.Component {
         fetch("http://localhost:4000/api/SearchAHero?name="+this.props.match.params.name)
             .then(results => results.json())
             .then(results => {
-                console.log(results);
-                this.state = {
+                this.setState({
                     character: results.map( character => {
+
+                        character.imageUrl = '/img/characters/' + character.imageUrl;
+                        character.logoUrl = '/img/logo/' + character.logoUrl;
+
+                        character.byLine = character.byLine ? <p>{character.byLine}</p> : null;
+                        console.log(character.byLine);
+
                         return (
-                            <div className={character.name} />
+                            <div className="single-character-div" key={character._id}>
+                                <div className="image-container">
+                                    <img src={character.imageUrl} />
+                                </div>
+                                <div className="text-container">
+                                    <img src={character.logoUrl} />
+                                    <h1>{character.name}</h1>
+                                    {character.byLine}
+                                    <h3>{character.alias}</h3>
+                                    <p>{character.summary}</p>
+                                </div>
+                                <div className="extra-container">
+                                    <p>{character.description}</p>
+                                    <ol>Powers: {character.powers.split(",").map(power => <li>{power}</li>)}</ol>
+                                    <p>{character.firstAppearance}</p>
+                                    <p>Related Characters: {character.relatedCharacters}</p>
+                                </div>
+                            </div>
                         )
                     })
-                }
+                })
             }, error => {
                 this.setState = error;
             })
     }
 
     render() {
+        console.log(this.state.character);
         return(
             <div>
-                <h1>Hero</h1>
                 {this.state.character}
             </div>
         )
